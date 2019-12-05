94f7f9180723a903e11c683acd6197c3970f7567
0_Nocaudie <o.nocaudie@googlemail.com>
2019-12-05 11:49:26 +0100

    yeah bru

--- src/kata/Chmod.java
+++ src/kata/Chmod.java
@@ -1,8 +1,45 @@
+package kata;
+
+import java.util.Arrays;
+
 public class Chmod {
 
     public static String convert(String numerical) {
+        StringBuilder s = new StringBuilder();
+        String[] bin = decToBin(numerical);
+        for (int i = 0; i < bin.length; i++) {
+            for (int j = 0; j < bin[i].length(); j++) {
+                if (j == 0 && bin[i].charAt(j) == '1') {
+                    s.append("r");
+                }
+                else if (j == 1 && bin[i].charAt(j) == '1')  {
+                    s.append("w");
+                }
+                else if (j == 2 && bin[i].charAt(j) == '1')  {
+                    s.append("x");
+                } else {s.append("-");}
+            }
+        }
 
-        // TODO
-        return null;
+        return s.toString();
+    }
+
+    public static String[] decToBin(String numerical) {
+        int[] tab = new int[numerical.length()];
+        String[] bin = new String[3];
+        for (int i = 0; i < numerical.length(); i++) {
+            tab[i] = Integer.parseInt("" + numerical.charAt(i));
+            bin[i] = Integer.toBinaryString(tab[i]);
+            switch (bin[i].length()) {
+                case 1:
+                    bin[i] = "00" + bin[i];
+                    break;
+                case 2:
+                    bin[i] = "0" + bin[i];
+                    break;
+            }
+        }
+        return bin;
+
     }
 }
--- src/test/ChmodTest.java
+++ src/test/ChmodTest.java
@@ -1,3 +1,4 @@
+import kata.Chmod;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,5 +9,38 @@ public class ChmodTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+/*    @Test
+    public void testDecToBin000() throws Exception {
+        Assert.assertEquals("000000000", Chmod.decToBin("000"));
+    }
+
+    @Test
+    public void testDecToBin001() throws Exception {
+        Assert.assertEquals("001001001", Chmod.decToBin("111"));
+    }
+
+    @Test
+    public void testDecToBin002() throws Exception {
+        Assert.assertEquals("000000010", Chmod.decToBin("002"));
+    }
+
+    @Test
+    public void testDecToBin777() throws Exception {
+        Assert.assertEquals("111111111", Chmod.decToBin("777"));
+    }
+
+    @Test
+    public void testDecToBin456() throws Exception {
+        Assert.assertEquals("100101110", Chmod.decToBin("456"));
+    }
+*/
+    @Test
+    public void testDecToBinrwxrwxrwx() throws Exception {
+        Assert.assertEquals("rwxrwxrwx", Chmod.convert("777"));
+    }
+
+    @Test
+    public void testDecToBinToutTiret() throws Exception {
+        Assert.assertEquals("---------", Chmod.convert("000"));
+    }
 }
